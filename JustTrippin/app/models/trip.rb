3bdf0603ac293a5fac635d0acaf69a4c8a19ea14
Brandon <bchansf@gmail.com>
2016-02-26 22:59:09 -0800

    render json of overview to index will move to models later

--- JustTrippin/app/models/trip.rb
+++ JustTrippin/app/models/trip.rb
@@ -1,3 +1,25 @@
 class Trip < ActiveRecord::Base
+    require 'httparty'
+  def self.overview
+    # response = HTTParty.post("http://terminal2.expedia.com:80/x/flights/overview/get",
+    #   :body => {
+    #             "MessageHeader": { "ClientInfo": { "DirectClientName": "Hackathon"}, "TransactionGUID": ""},
+    #             "tpid": 1, "eapid": 0, "PointOfSaleKey": { "JurisdictionCountryCode": "USA", "CompanyCode": "10111", "ManagementUnitCode": "1010" },
 
+    #             "OriginAirportCodeList": {
+    #             "AirportCode": ["JFK"]
+    #             },
+    #             "DestinationAirportCodeList": {
+    #             "AirportCode": ["SFO", "OAK"]
+    #             },
+
+    #             "FlightListings": { }
+    #             }.to_json,
+    #   :headers => { 'Content-Type' => 'application/json',
+    #                 'Accept' => 'application/json',
+    #               'Authorization' => ENV['EXPEDIA_SECRET_KEY']
+    #               }
+    #   )
+    # return response
+  end
 end
